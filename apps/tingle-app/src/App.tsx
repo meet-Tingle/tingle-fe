6aa7d28a41d344d6385c20a10f98c2c9301a1074
jongbin26 <010jjj@naver.com>
2025-10-19 03:41:35 +0900

    chore: biome 기본 세팅 추가

--- apps/tingle-app/src/App.tsx
+++ apps/tingle-app/src/App.tsx
@@ -1,37 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import tingleLogo from "./assets/tingle.svg";
+import PWABadge from "./PWABadge.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="tingle logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={tingleLogo} className="logo tingle" alt="Tingle logo" />
       </div>
-      <h1>tingle</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>TINGLE</h1>
+      <p className="description">
+        Tingle is a platform for university students to find and connect with
+        other students.
       </p>
       <PWABadge />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
