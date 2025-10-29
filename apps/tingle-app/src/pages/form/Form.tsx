5447d0bdfef4e459bb06714e9104428caa7630b5
jongbin26 <010jjj@naver.com>
2025-10-29 22:22:04 +0900

    chore: parser 패키지 추가

--- apps/tingle-app/src/pages/form/Form.tsx
+++ apps/tingle-app/src/pages/form/Form.tsx
@@ -1,9 +1,10 @@
+import { createParser } from "@tingle/parser";
 import { Text } from "@tingle/ui";
 
 export default function Form() {
   return (
     <Text size="2xl" weight="bold" color="gray_600">
-      Form Page
+      {createParser()}
     </Text>
   );
 }
