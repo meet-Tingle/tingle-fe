632a6d366af9ed291eee54fe217024e39316181a
jongbin26 <010jjj@naver.com>
2025-10-29 23:27:00 +0900

    feat: 등록한 validation 기반으로 metadata 파싱하는 Parser 클래스 추가

--- packages/parser/Parser.ts
+++ packages/parser/Parser.ts
@@ -1,3 +1,78 @@
-export const createParser = () => {
-  return "parser";
+import { v4 as uuidv4 } from "uuid";
+import type { Metadata, ValidationFunction } from "./parser.type";
+import { defaultValidatorRegistry, ValidatorFactory } from "./validators";
+import { CustomValidator } from "./validators/common/CustomValidator";
+
+type ParsedField = {
+  id: string;
+  validate: ValidationFunction;
 };
+
+interface ParserInterface {
+  parse(metadata: Metadata[]): ParsedField[];
+}
+
+class Parser implements ParserInterface {
+  private validatorFactory: ValidatorFactory;
+  private customValidations = new Map<string, ValidationFunction>();
+
+  constructor(validatorFactory?: ValidatorFactory) {
+    this.validatorFactory =
+      validatorFactory ?? new ValidatorFactory(defaultValidatorRegistry);
+  }
+
+  public registerCustomValidation(
+    name: string,
+    validationFn: ValidationFunction,
+  ): void {
+    this.customValidations.set(name, validationFn);
+  }
+
+  public parse(metadata: Metadata[]) {
+    return metadata.map((data) => this.parseField(data));
+  }
+
+  private parseField(field: Metadata): ParsedField {
+    const id = this.generateId();
+    const validate = this.createValidationFunction(field);
+    return {
+      id,
+      validate,
+    };
+  }
+
+  private generateId(): string {
+    return uuidv4();
+  }
+
+  private createValidationFunction(field: Metadata): ValidationFunction {
+    const validators = this.validatorFactory.create(field);
+
+    // custom validation 등록
+    if (field.validation?.custom) {
+      validators.push(
+        new CustomValidator(field.validation.custom, this.customValidations),
+      );
+    }
+
+    return (value) => {
+      const errors: string[] = [];
+
+      // 모든 validator 검증하여 에러 수집
+      for (const validator of validators) {
+        const result = validator.validate(value);
+        if (!result.isValid && result.error) {
+          errors.push(result.error);
+        }
+      }
+
+      return errors.length > 0
+        ? { isValid: false, error: errors[0], errors }
+        : { isValid: true };
+    };
+  }
+}
+
+export function createParser(validatorFactory?: ValidatorFactory): Parser {
+  return new Parser(validatorFactory);
+}
--- packages/parser/index.ts
+++ packages/parser/index.ts
@@ -1,1 +1,9 @@
 export * from "./Parser";
+export * from "./parser.type";
+export type { Validator, ValidatorConstructor } from "./validators";
+
+export {
+  defaultValidatorRegistry,
+  ValidatorFactory,
+  ValidatorRegistry,
+} from "./validators";
