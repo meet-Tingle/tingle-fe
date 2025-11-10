6c031f535374fca415fcbaefe197694a0bb0cd80
jongbin26 <010jjj@naver.com>
2025-11-10 23:18:28 +0900

    feat: AuthProvider 위치 수정

--- apps/tingle-app/src/routes/__root.tsx
+++ apps/tingle-app/src/routes/__root.tsx
@@ -1,15 +1,15 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 // import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { AuthProvider } from "@/provider/AuthProvider";
 import { appContainer } from "./root.css";
 
 const RootLayout = () => {
   return (
-    <>
-      <div className={appContainer}>
+    <div className={appContainer}>
+      <AuthProvider>
         <Outlet />
-      </div>
-      {/* <TanStackRouterDevtools /> */}
-    </>
+      </AuthProvider>
+    </div>
   );
 };
 
--- apps/tingle-app/src/routes/_withAuth.tsx
+++ apps/tingle-app/src/routes/_withAuth.tsx
@@ -1,14 +1,9 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import { AuthProvider } from "@/provider/AuthProvider";
 
 export const Route = createFileRoute("/_withAuth")({
   component: AuthenticatedLayout,
 });
 
 function AuthenticatedLayout() {
-  return (
-    <AuthProvider>
-      <Outlet />
-    </AuthProvider>
-  );
+  return <Outlet />;
 }
